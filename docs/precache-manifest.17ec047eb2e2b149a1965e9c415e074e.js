d43d68ca95dda3517bc68204b6a9cee970c6bd0b
Leandro Bazan <lbazan57@yahoo.com>
2020-09-24 17:06:40 -0300

    docs agregado

---
+++ docs/precache-manifest.17ec047eb2e2b149a1965e9c415e074e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16d12137df8ffa2ef2efbeec053b6a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22e91f2e573c4c6069e4",
+    "url": "/static/css/main.37bea27c.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22e91f2e573c4c6069e4",
+    "url": "/static/js/main.bc81cfce.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
